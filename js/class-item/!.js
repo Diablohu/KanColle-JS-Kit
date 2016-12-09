7d5c7be1b34aa151c962691b699c57a51cea670f
Diablohu <diablohudream@gmail.com>
2016-12-10 05:47:56 +0800

    update ItemBase

--- js/class-item/!.js
+++ js/class-item/!.js
@@ -1,11 +1,12 @@
 class ItemBase {
-	constructor() {
+	constructor(data) {
+		$.extend(true, this, data)
 	}
 
 	getName(language){
 		language = language || _g.lang
 		return this['name']
-				? (this['name'][language] || this['name'])
+				? (this['name'][language] || this['name'].ja_jp || this.name)
 				: null
 	}
 	
